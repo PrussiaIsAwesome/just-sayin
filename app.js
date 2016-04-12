ccd0973dc8e5f32d41b26cfdd585235f1250fa15
Prussia <akame13579@gmail.com>
2016-04-12 22:42:47 +0000

    week-10_2

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-{
   var clicks = 0;
 
   function onClick() {
@@ -10,7 +9,7 @@
     var name = $("#name").val();
     if (name != null) {
       document.getElementById("demo").innerHTML =
-        "\"Hello " + name + "! I just want to say...I 'M AWESOME!!!! BWAHAHAHAHAHAHAHAHAHAH!!!!!!\"";
+        "\"Hello " + name + "! I just want to say...I'M AWESOME!!!! BWAHAHAHAHAHAHAHAHAHAH!!!!!!\"";
       onClick();
     }
   }
@@ -18,29 +17,51 @@
   function my2ndFunction() {
     var comment = $("#comment").val();
     if (comment != null) {
-      document.getElementById("demo").innerHTML = comment + "ThAnKs FoR tHe CoMmEnT!!!";
+      document.getElementById("demo").innerHTML =
+        comment + "ThAnKs FoR tHe CoMmEnT!!!";
       onClick();
     }
   }
+
   $(function() {
     new WOW().init();
   });
+
   var count = 0;
 
   function mainAlert() {
     var name = $('#name').val();
+
     alert(name + ", I'M THE QUEEN!");
+
     count++;
+
     $('countAlert').show();
+
     $('#countAlert #number').text(count);
   }
- 
-
-  function my3ndFunction() {
+  
+   function my4thFunction() {
     var button = $("#button").val();
     if (button != null) {
-      $(".intro").hide();
-      $(".gameIntro").show();
+      document.getElementById("demo").innerHTML =
+       $(".intro").hide()
+       $(".gameIntro").show()
+      onClick();
     }
   }
-}
+  
+  $('.gameIntro').hide();
+  
+   function my3ndFunction() {
+    var button2 = $("#button2").val();
+    if (button2 != null) {
+      document.getElementById("demo").innerHTML =
+       
+      onClick();
+    }
+  }
+  
+  
+  
+  
