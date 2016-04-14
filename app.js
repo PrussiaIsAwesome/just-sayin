e97ed44a60e6195738448c96f063803562c8c2da
Prussia <akame13579@gmail.com>
2016-04-14 00:30:11 +0000

    week-12_1

--- app.js
+++ app.js
@@ -1,67 +1,95 @@
-  var clicks = 0;
+  {
+    var clicks = 0;
 
-  function onClick() {
-    clicks += 1;
-    document.getElementById("clicks").innerHTML = clicks;
-  }
-
-  function myFunction() {
-    var name = $("#name").val();
-    if (name != null) {
-      document.getElementById("demo").innerHTML =
-        "\"Hello " + name + "! I just want to say...I'M AWESOME!!!! BWAHAHAHAHAHAHAHAHAHAH!!!!!!\"";
-      onClick();
+    function onClick() {
+      clicks += 1;
+      document.getElementById("clicks").innerHTML = clicks;
     }
-  }
 
-  function my2ndFunction() {
-    var comment = $("#comment").val();
-    if (comment != null) {
-      document.getElementById("demo").innerHTML =
-        comment + "ThAnKs FoR tHe CoMmEnT!!!";
-      onClick();
+    function myFunction() {
+      var name = $("#name").val();
+      if (name != null) {
+        document.getElementById("demo").innerHTML =
+          "\"Hello " + name + "! I just want to say...I'M AWESOME!!!! BWAHAHAHAHAHAHAHAHAHAH!!!!!!\"";
+
+      }
     }
-  }
 
-  $(function() {
-    new WOW().init();
-  });
+    function my2ndFunction() {
+      var comment = $("#comment").val();
+      if (comment != null) {
+        document.getElementById("demo").innerHTML = comment + "ThAnKs FoR tHe CoMmEnT!!!";
 
-  var count = 0;
-
-  function mainAlert() {
-    var name = $('#name').val();
-
-    alert(name + ", I'M THE QUEEN!");
-
-    count++;
-
-    $('countAlert').show();
-
-    $('#countAlert #number').text(count);
-  }
-  
-   function my4thFunction() {
-    var button = $("#button").val();
-    if (button != null) {
-      document.getElementById("demo").innerHTML =
-       $(".intro").hide()
-       $(".gameIntro").show()
-      onClick();
+      }
     }
-  }
-  
-  $('.gameIntro').hide();
-  
-   function my3ndFunction() {
-    var button2 = $("#button2").val();
-    if (button2 != null) {
-      document.getElementById("demo").innerHTML =
-       
-      onClick();
+
+    $(function() {
+      new WOW().init();
+    });
+
+    var count = 0;
+
+    function mainAlert() {
+      var name = $('#name').val();
+
+      alert(name + ", I'M THE QUEEN!");
+
+      count++;
+
+      $('countAlert').show();
+
+      $('#countAlert #number').text(count);
     }
+
+    $(".game-intro").hide();
+    $(".game").hide();
+    $(".game-forward").hide();
+    $(".forwards").hide();
+    $(".game-end").hide();
+    $(".end").hide();
+
+    function my4thFunction() {
+      $(".game-intro").show();
+      $(".intro1").hide();
+      $(".intro2").hide();
+    }
+
+
+    function my3ndFunction() {
+      $(".game-intro").hide();
+      $(".game").show();
+    }
+    
+    function my5thFunction() {
+      $(".game-forward").show();
+      $(".game").hide();
+    }
+  
+     function my6thFunction() {
+      $(".forwards").show();
+      $(".game-forward").hide();
+    }
+    
+     function my7thFunction() {
+      $(".game-end").show();
+      $(".forwards").hide();
+    }
+    
+    function my8thFunction() {
+      $(".end").show();
+      $(".game-end").hide();
+    }
+    
+      
+    
+    
+  
+  
+  
+  
+  
+  
+  
+  
   }
-  
-  
-  
   
